c452e3f54fca5d8e3b025f7e72a3a9b3f830226e
Derevenskikh Aleksander <hedgehog@kartis.ru>
2022-11-26 12:13:56 +0300

    add project files

---
+++ Mellionare/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mellionare
+//
+//  Created by Aleksandr Derevenskih on 26.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
