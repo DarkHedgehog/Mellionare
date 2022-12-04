1e6d5eba5d7ff6aee53b68de8ad6b8862a29eb24
Derevenskikh Aleksander <hedgehog@kartis.ru>
2022-12-04 12:02:37 +0300

    edit form of questions

--- Mellionare/Settings/QuestionEdit/QuestionsEditViewController.swift
+++ Mellionare/Settings/QuestionEdit/QuestionsEditViewController.swift
@@ -11,12 +11,38 @@ class QuestionsEditViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
 
+    @IBAction func addQuestion(_ sender: Any) {
+        let emptyQuestionRecord = GameQuestion(
+            id: genQuestionId(),
+            text: "",
+            answers: [
+                GameAnswerVariant(id: 0, text: ""),
+                GameAnswerVariant(id: 1, text: ""),
+                GameAnswerVariant(id: 2, text: ""),
+                GameAnswerVariant(id: 3, text: "")
+            ],
+            correctAnswerId: 0
+        )
+        questions.append(emptyQuestionRecord)
+        tableView.reloadData()
+    }
+
+    var questions: [GameQuestion] = []
+
+    private var questionIdGen = 0;
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        questionIdGen = Game.shared.questionCustomData.map { $0.id }.max() ?? 0
         tableView.dataSource = self
         tableView.delegate = self
         tableView.allowsSelection = false
+    }
+
+    private func genQuestionId() -> Int {
+        questionIdGen += 1
+        return questionIdGen
     }
 }
 
@@ -25,10 +51,11 @@ extension QuestionsEditViewController: UITableViewDelegate {
         return CGFloat(280)
     }
 }
+
 extension QuestionsEditViewController: UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return questions.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -38,8 +65,10 @@ extension QuestionsEditViewController: UITableViewDataSource {
             preconditionFailure("Error cast to AnswerTableViewCell")
         }
 
-        cell.questionText.text = ""
-        cell.answersVariations.prepareElements(["", "", "", ""], selectedIndex: 1)
+        let question = questions[indexPath.row]
+        cell.questionText.text = question.text
+        let answersStrings = question.answers.map { $0.text }
+        cell.answersVariations.prepareElements(answersStrings, selectedIndex: question.correctAnswerId)
         return cell
     }
 }
